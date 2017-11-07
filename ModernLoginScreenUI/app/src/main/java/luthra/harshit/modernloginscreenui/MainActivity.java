62fb0412be333da268bc6ef585f5c7ba4d8ea01d
Harshit  Luthra <love.coolsachin@gmail.com>
2017-11-07 20:10:35 +0530

    Add files via upload

---
+++ ModernLoginScreenUI/app/src/main/java/luthra/harshit/modernloginscreenui/MainActivity.java
@@ -0,0 +1,13 @@
+package luthra.harshit.modernloginscreenui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
